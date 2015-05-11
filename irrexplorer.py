8f39946fa37c31786144e8aa68e7af55bf8ab230
Job Snijders <job@instituut.net>
2015-05-11 21:10:50 +0200

    Change color to bootstrap label

--- irrexplorer.py
+++ irrexplorer.py
@@ -323,25 +323,26 @@ def prefix_report(prefix):
         else:
             prefixes[p]['ripe_managed'] = False
 
+    # default, primary, succes, info, warning, danger
     for p in prefixes:
         if prefixes[p]['ripe_managed'] \
                 and not prefixes[p]['ripe'] \
                 and not prefixes[p]['bgp_origin']:
             prefixes[p]['advise'] = "Register this route-object in the RIPE DB, consider announcing it"
-            prefixes[p]['color'] = "orange"
+            prefixes[p]['label'] = "warning"
         elif prefixes[p]['ripe_managed'] \
             and not prefixes[p]['ripe'] \
             and prefixes[p]['bgp_origin']:
             prefixes[p]['advise'] = "Prefix is in DFZ, but registered in RIPE!"
-            prefixes[p]['color'] = "red"
+            prefixes[p]['label'] = "danger"
         elif prefixes[p]['ripe_managed'] \
             and prefixes[p]['ripe'] is not prefixes[p]['bgp_origin']:
             prefixes[p]['advise'] = "Prefix is in DFZ, but registered with wron origin in RIPE!"
-            prefixes[p]['color'] = "red"
+            prefixes[p]['label'] = "danger"
         elif prefixes[p]['ripe_managed'] \
             and prefixes[p]['ripe'] is not prefixes[p]['bgp_origin']:
             prefixes[p]['advise'] = "Prefix is in DFZ, but registered with wron origin in RIPE!"
-            prefixes[p]['color'] = "red"
+            prefixes[p]['label'] = "danger"
 
     return prefixes
 
