51b4ece98d0efe8cf1e7d691f52e6d7e02fa0c60
Henrik Thostrup Jensen <htj@nordu.net>
2015-06-24 14:38:57 +0200

    move prefix advice into seperate function to make things a bit more comprehensible

--- irrexplorer/report.py
+++ irrexplorer/report.py
@@ -29,109 +29,19 @@
 import time
 
 
-
-class NoPrefixError(Exception):
-    pass
-
+SOURCE = 'source'
+BGP = 'bgp'
 
 # we should generate these from the database sometime...
 IRR_DBS = ['afrinic', 'altdb', 'apnic', 'arin', 'bboi', 'bell', 'gt', 'jpirr', 'level3', 'nttcom', 'radb', 'rgnet', 'savvis', 'tc', 'ripe']
 
 
+class NoPrefixError(Exception):
+    pass
 
-def prefix_report(pgdb, prefix, exact=False):
-    """
-        - find least specific
-        - search in BGP for more specifics
-        - search in IRR for more specifics
-        - check all prefixes whether they are RIPE managed or not
-        - return dict
-    """
 
-    SOURCE = 'source'
-    BGP = 'bgp'
 
-    t_start = time.time()
-
-    print
-    print 'Prefix report: %s, exact=%s' % (prefix, exact)
-
-    routes = pgdb.query_prefix(prefix, exact=False)
-    prefixes = {}
-    for pfx, asn, source in routes:
-        ps = prefixes.setdefault(pfx, {}).setdefault(SOURCE, {})
-        ps.setdefault(source, []).append(asn)
-
-    for pfx_data in prefixes.values():
-        if BGP in pfx_data[SOURCE]:
-            pfx_data[BGP] = pfx_data[SOURCE].pop(BGP)
-
-    print 'Prefixes:'
-    for pd in prefixes.items():
-        print pd
-
-    # TODO if we find any prefixes larger than the inputted one, we should find prefixes covered by that prefixes
-    # Go through the prefixes, find the shortest one, if it is different from the inputted one, do another search
-
-#    if exact:
-#        bgp_specifics = other_query("BGP", "search_exact", prefix)
-#        irr_specifics = irr_query("search_exact", prefix)
-#    else:
-#        tree = radix.Radix()
-#        bgp_aggregate = other_query("BGP", "search_aggregate", prefix)
-#        if bgp_aggregate:
-#            bgp_aggregate = bgp_aggregate[0]
-#            tree.add(bgp_aggregate)
-#        irr_aggregate = irr_query("search_aggregate", prefix)
-#        for r in irr_aggregate:
-#            if irr_aggregate[r]:
-#                tree.add(irr_aggregate[r][0])
-#        aggregate = tree.search_worst(prefix)
-#        if not aggregate:
-#            raise NoPrefixError("Could not find any matching prefix in IRR or BGP tables for %s" % prefix)
-#
-#        aggregate = aggregate.prefix
-#
-#        bgp_specifics = other_query("BGP", "search_specifics", aggregate)
-#        irr_specifics = irr_query("search_specifics", aggregate)
-
-#    prefixes = {}
-#    for p in bgp_specifics:
-#        if p not in prefixes:
-#            prefixes[p] = {'bgp_origin': bgp_specifics[p]['origins']}
-#        else:
-#            prefixes[p]['bgp_origin'] = bgp_specifics[p]['origins']
-#    for db in irr_specifics:
-#        if irr_specifics[db]:
-#            for p in irr_specifics[db]:
-#                if p not in prefixes:
-#                    prefixes[p] = {}
-#                    prefixes[p]['bgp_origin'] = False
-#
-#    """
-#    irr_specifics looks like:
-#        {'apnic': {}, 'gt': {}, 'bboi': {}, 'radb': {}, 'jpirr': {},
-#        'bell': {}, 'altdb': {}, 'rgnet': {}, 'savvis': {}, 'level3': {},
-#        'ripe': {'85.184.0.0/16': {'origins': [8935]}},
-#        'arin': {}, 'afrinic': {}, 'tc': {}}
-#    """
-#    print "prefix_report: Lookup prefixes: %s" % prefixes
-#    print "prefix_report: Lookup irr_specifics: %s" % irr_specifics
-#
-#    for db in irr_specifics:
-#        # set all db sources to False initially, later fill them
-#        for p in prefixes:
-#            prefixes[p][db] = False
-#        for p in irr_specifics[db]:
-#            prefixes.setdefault(p, {})[db] = irr_specifics[db][p]['origins']
-
-    for pfx, pfx_data in prefixes.items():
-        managed_routes = pgdb.query_managed_prefix(pfx) # this can be done better
-        print 'Managed routes for prefix %s --> %s' % (pfx, managed_routes)
-        # we only do ripe managed routes at the moment, so if/else if fine
-        managed = True if managed_routes else False
-        pfx_data['ripe_managed'] = managed
-
+def add_prefix_advice(prefixes):
 
     # default, primary, succes, info, warning, danger
     for pfx, pfx_data in prefixes.items():
@@ -221,6 +131,103 @@ def prefix_report(pgdb, prefix, exact=False):
         else: # not ripe managed, no bgp origin
             pfx_data['advice'] = "Not seen in BGP, but (legacy?) route-objects exist, consider clean-up"
             pfx_data['label'] = "warning"
+
+
+    return prefixes
+
+
+
+def prefix_report(pgdb, prefix, exact=False):
+    """
+        - find least specific
+        - search in BGP for more specifics
+        - search in IRR for more specifics
+        - check all prefixes whether they are RIPE managed or not
+        - return dict
+    """
+
+    t_start = time.time()
+
+    print
+    print 'Prefix report: %s, exact=%s' % (prefix, exact)
+
+    routes = pgdb.query_prefix(prefix, exact=False)
+    prefixes = {}
+    for pfx, asn, source in routes:
+        ps = prefixes.setdefault(pfx, {}).setdefault(SOURCE, {})
+        ps.setdefault(source, []).append(asn)
+
+    for pfx_data in prefixes.values():
+        if BGP in pfx_data[SOURCE]:
+            pfx_data[BGP] = pfx_data[SOURCE].pop(BGP)
+
+    print 'Prefixes:'
+    for pd in prefixes.items():
+        print pd
+
+    # TODO if we find any prefixes larger than the inputted one, we should find prefixes covered by that prefixes
+    # Go through the prefixes, find the shortest one, if it is different from the inputted one, do another search
+
+#    if exact:
+#        bgp_specifics = other_query("BGP", "search_exact", prefix)
+#        irr_specifics = irr_query("search_exact", prefix)
+#    else:
+#        tree = radix.Radix()
+#        bgp_aggregate = other_query("BGP", "search_aggregate", prefix)
+#        if bgp_aggregate:
+#            bgp_aggregate = bgp_aggregate[0]
+#            tree.add(bgp_aggregate)
+#        irr_aggregate = irr_query("search_aggregate", prefix)
+#        for r in irr_aggregate:
+#            if irr_aggregate[r]:
+#                tree.add(irr_aggregate[r][0])
+#        aggregate = tree.search_worst(prefix)
+#        if not aggregate:
+#            raise NoPrefixError("Could not find any matching prefix in IRR or BGP tables for %s" % prefix)
+#
+#        aggregate = aggregate.prefix
+#
+#        bgp_specifics = other_query("BGP", "search_specifics", aggregate)
+#        irr_specifics = irr_query("search_specifics", aggregate)
+
+#    prefixes = {}
+#    for p in bgp_specifics:
+#        if p not in prefixes:
+#            prefixes[p] = {'bgp_origin': bgp_specifics[p]['origins']}
+#        else:
+#            prefixes[p]['bgp_origin'] = bgp_specifics[p]['origins']
+#    for db in irr_specifics:
+#        if irr_specifics[db]:
+#            for p in irr_specifics[db]:
+#                if p not in prefixes:
+#                    prefixes[p] = {}
+#                    prefixes[p]['bgp_origin'] = False
+#
+#    """
+#    irr_specifics looks like:
+#        {'apnic': {}, 'gt': {}, 'bboi': {}, 'radb': {}, 'jpirr': {},
+#        'bell': {}, 'altdb': {}, 'rgnet': {}, 'savvis': {}, 'level3': {},
+#        'ripe': {'85.184.0.0/16': {'origins': [8935]}},
+#        'arin': {}, 'afrinic': {}, 'tc': {}}
+#    """
+#    print "prefix_report: Lookup prefixes: %s" % prefixes
+#    print "prefix_report: Lookup irr_specifics: %s" % irr_specifics
+#
+#    for db in irr_specifics:
+#        # set all db sources to False initially, later fill them
+#        for p in prefixes:
+#            prefixes[p][db] = False
+#        for p in irr_specifics[db]:
+#            prefixes.setdefault(p, {})[db] = irr_specifics[db][p]['origins']
+
+    for pfx, pfx_data in prefixes.items():
+        managed_routes = pgdb.query_managed_prefix(pfx) # this can be done better
+        print 'Managed routes for prefix %s --> %s' % (pfx, managed_routes)
+        # we only do ripe managed routes at the moment, so if/else if fine
+        managed = True if managed_routes else False
+        pfx_data['ripe_managed'] = managed
+
+    add_prefix_advice(prefixes)
 
     print 'Advice:'
     for p,d in prefixes.items():
