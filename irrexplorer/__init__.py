461cde3f3c17019d6b3f199282d2535db25cd3bf
Henrik Thostrup Jensen <htj@nordu.net>
2015-07-09 14:33:07 +0200

    remove some unused code in the irrexplorer module

--- irrexplorer/__init__.py
+++ irrexplorer/__init__.py
@@ -6,11 +6,3 @@ __license__     = "BSD 2-Clause"
 __status__      = "Development"
 __url__         = "https://github.com/job/irrexplorer"
 
-import yaml
-
-class config(object):
-    """config stub object for nrtm testing"""
-    def __init__(self, cfgfile):
-        data = yaml.load(open(cfgfile))
-        self.databases = data['databases']
-
